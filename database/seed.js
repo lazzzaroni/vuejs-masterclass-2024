66eb8fd07fa4b569685372ddc2850188446de750
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-16 14:40:19 +0100

    setup faker-js & Supabase Client

---
+++ database/seed.js
@@ -0,0 +1,7 @@
+import { faker } from '@faker-js/faker'
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+console.log(supabase)

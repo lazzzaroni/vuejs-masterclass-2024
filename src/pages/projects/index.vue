6ae25104fd03ffbe2f91f10c7bd43cd501a7b70a
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-16 15:32:36 +0100

    add type definition for ref

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,7 +2,7 @@
 import { supabase } from '@/lib/supabaseClient'
 import { onMounted, ref } from 'vue'
 
-const projects = ref()
+const projects = ref<any[] | null>(null)
 
 onMounted(async () => {
   const { data, error } = await supabase.from('projects').select()
@@ -17,6 +17,10 @@ onMounted(async () => {
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Home</RouterLink>
-    <pre>{{ projects }}</pre>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>

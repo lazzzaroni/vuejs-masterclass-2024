9c6b1e129fce22beddfa78885a175f98ea1b6be4
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-16 15:17:04 +0100

    query Supabase from component

--- database/seed.js
+++ database/seed.js
@@ -1,10 +1,21 @@
-import { faker } from '@faker-js/faker'
-
+import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`,
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
 const seedProjects = async (numberOfEntries = 1) => {
+  logStep('Seeding projects...')
   const projects = []
 
   for (let i = 0; i < numberOfEntries; i++) {
@@ -18,7 +29,19 @@ const seedProjects = async (numberOfEntries = 1) => {
     })
   }
 
-  await supabase.from('projects').insert(projects)
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
 }
 
-await seedProjects(10)
+const seedDatabase = async (numEntriesPerTable) => {
+  await seedProjects(numEntriesPerTable)
+}
+
+const NUM_ENTRIES_PER_TABLE = 10
+
+seedDatabase(NUM_ENTRIES_PER_TABLE)

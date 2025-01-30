dfe9f03f0f1e3d63ff2e27e1e90c427b0df05c5c
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-30 11:28:41 +0100

    add prettier prettier-plugin-tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+  <nav class="bg-muted/40 flex h-16 items-center justify-between border-b px-6">
     <form class="w-full max-w-96">
-      <input type="text" placeholder="Search..." class="w-full pl-8 bg-background" />
+      <input type="text" placeholder="Search..." class="bg-background w-full pl-8" />
     </form>
     <div class="size-8 rounded-full bg-white"></div>
   </nav>

cbb59737039e7204dc3eefde8536773d4ea625fb
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-30 13:36:01 +0100

    refactor Sidebar

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SidebarLinks from '@/components/Layout/SidebarLinks.vue'
+
+const links = [
+  { title: 'Dashboard', to: '/', icon: 'lucide:house' },
+  { title: 'Projects', to: '/projects', icon: 'lucide:building-2' },
+  { title: 'My Tasks', to: '/tasks', icon: 'lucide:badge-check' },
+]
+const accountLinks = [
+  { title: 'Profile', to: '/profile', icon: 'lucide:user' },
+  { title: 'Settings', to: '/settings', icon: 'lucide:settings' },
+  { title: 'Sign out', to: '/signout', icon: 'lucide:log-out' },
+]
+</script>
 
 <template>
   <aside
@@ -6,61 +19,21 @@
   >
     <div class="flex h-16 shrink-0 items-center justify-between gap-1 border-b px-2 lg:px-4">
       <button variant="outline" size="icon" class="h-8 w-8">
-        <iconify-icon icon="lucide:menu"></iconify-icon>
+        <iconify-icon icon="lucide:menu" />
       </button>
 
       <button variant="outline" size="icon" class="h-8 w-8">
-        <iconify-icon icon="lucide:plus"></iconify-icon>
+        <iconify-icon icon="lucide:plus" />
       </button>
     </div>
 
     <nav class="relative flex h-full flex-col justify-between gap-2">
       <div>
-        <RouterLink
-          to="/"
-          class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
-        >
-          <iconify-icon icon="lucide:house"></iconify-icon>
-          <span class="hidden text-nowrap lg:block">Dashboard</span>
-        </RouterLink>
-        <RouterLink
-          to="/projects"
-          class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
-        >
-          <iconify-icon icon="lucide:building-2"></iconify-icon>
-          <span class="hidden text-nowrap lg:block">Projects</span>
-        </RouterLink>
-        <RouterLink
-          to="/tasks"
-          class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
-        >
-          <iconify-icon icon="lucide:badge-check"></iconify-icon>
-          <span class="hidden text-nowrap lg:block">My Tasks</span>
-        </RouterLink>
+        <SidebarLinks :links="links" />
       </div>
 
       <div class="bg-background border-y py-3 text-center">
-        <RouterLink
-          to="/profile"
-          class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
-        >
-          <iconify-icon icon="lucide:user"></iconify-icon>
-          <span class="hidden text-nowrap lg:block">Profile</span>
-        </RouterLink>
-        <RouterLink
-          to="/settings"
-          class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
-        >
-          <iconify-icon icon="lucide:settings"></iconify-icon>
-          <span class="hidden text-nowrap lg:block">Settings</span>
-        </RouterLink>
-        <RouterLink
-          to="/signout"
-          class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
-        >
-          <iconify-icon icon="lucide:log-out"></iconify-icon>
-          <span class="hidden text-nowrap lg:block">Sign out</span>
-        </RouterLink>
+        <SidebarLinks :links="accountLinks" />
       </div>
     </nav>
   </aside>
---
+++ src/components/Layout/SidebarLinks.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface LinkProp {
+  title: string
+  to: string
+  icon: string
+}
+
+defineProps<{
+  links: Array<LinkProp>
+}>()
+</script>
+
+<template>
+  <RouterLink
+    exact-active-class="text-primary bg-muted"
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    class="hover:text-primary text-muted-foreground mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 transition-colors lg:justify-normal"
+  >
+    <iconify-icon :icon="link.icon" />
+    <span class="hidden text-nowrap lg:block">{{ link.title }}</span>
+  </RouterLink>
+</template>

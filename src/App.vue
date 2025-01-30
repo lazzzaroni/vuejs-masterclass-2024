dd66e7058a96752e5d660ae876aa781bf2bac8ad
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-30 11:28:25 +0100

    add Tailwind & shadcn-vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-console.log(supabase)
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input type="text" placeholder="Search..." class="w-full pl-8 bg-background" />
+    </form>
+    <div class="size-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>

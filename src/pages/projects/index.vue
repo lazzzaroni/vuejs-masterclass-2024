f2a359d90c490ecb11bf3e44720a818b5f59d1c4
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-30 14:25:30 +0100

    create DataTable for projects page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import { onMounted, ref } from 'vue'
+import { h, onMounted, ref } from 'vue'
 import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
 
 const projects = ref<Array<Tables<'projects'>> | null>(null)
 
@@ -12,16 +14,36 @@ onMounted(async () => {
 
   projects.value = data
 })
+
+const columns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators')),
+      )
+    },
+  },
+]
 </script>
 
 <template>
-  <div>
-    <h1>Projects Page</h1>
-    <RouterLink to="/">Home</RouterLink>
-    <ul>
-      <li v-for="project in projects" :key="project.id">
-        {{ project.name }}
-      </li>
-    </ul>
-  </div>
+  <DataTable v-if="projects" :columns="columns" :data="projects" />
 </template>

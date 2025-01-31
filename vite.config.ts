1d641eaadb877ff4fc1311c23d4e64abf089be7d
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-01-31 16:59:13 +0100

    integrate unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
@@ -11,6 +12,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     VueRouter(),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
     vue({
       template: {
         compilerOptions: {

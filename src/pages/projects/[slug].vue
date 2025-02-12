40862c7cdb035f48883b2caa75bca5061a2b6658
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-02-12 17:50:09 +0100

    update Project name dynamically

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -6,6 +6,13 @@ const route = useRoute('/projects/[slug]')
 
 const project = ref<Project | null>(null)
 
+watch(
+  () => project.value?.name,
+  () => {
+    usePageStore().pageData.title = `Project: ${project.value?.name || ''}`
+  },
+)
+
 onMounted(async () => {
   const { data, error } = await projectQuery(route.params.slug)
 

6dfd0b349f5ec9c2d31d63b9c455d460c25f66da
lazzzaroni <iamnotlazzaroni@gmail.com>
2025-02-12 16:58:29 +0100

    retrieve Project details for Task

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -9,7 +9,10 @@ usePageStore().pageData.title = 'My Tasks'
 const tasks = ref<Array<Tables<'tasks'>> | null>(null)
 
 onMounted(async () => {
-  const { data, error } = await supabase.from('tasks').select()
+  const { data, error } = await supabase.from('tasks').select(`
+  *,
+  projects (id, name, slug)
+  `)
 
   if (error) console.log(error)
 
@@ -46,10 +49,17 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     },
   },
   {
-    accessorKey: 'project_id',
+    accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.projects.slug}`,
+          class: 'text-left font-medium hover:bg-muted block w-full',
+        },
+        () => row.getValue('projects').name,
+      )
     },
   },
   {
